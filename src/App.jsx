3ff1a852b9cf70e07c25859cd38272ef325d8e24
Ibiscus2025 <anastasiapag86@gmail.com>
2025-10-07 20:07:59 +0300

    Delete src directory

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ padding: 24, fontFamily: "system-ui" }}>
-      <h1>Wine Finder Pool Bar ✅</h1>
-      <p>Αν βλέπεις αυτό, το React κάνει render σωστά.</p>
-    </div>
-  );
-}
